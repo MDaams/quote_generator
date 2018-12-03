59ce752162b6706ae8909b690fb2c4d2dd9755b7
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-03 19:08:30 +0100

    set up first test.

--- quote_generator/mainpage/functional_tests.py
+++ quote_generator/mainpage/functional_tests.py
@@ -6,7 +6,22 @@ import unittest
 class NewMainPageVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome('../util/chromedriver.exe')
+        self.browser = webdriver.Chrome('../../util/chromedriver.exe')
     
     def tearDown(self):
         self.browser.quit()
+
+        
+    def test_random_quote_is_shown(self):
+        self.browser.get('http://localhost:8000')
+        #user visits the mainpage for the first time. 
+        # Page title says 'Quote Generator'
+        self.assertIn('Quote Generator', self.browser.title)
+        # A random quote is displayed.
+        quote_div = driver.find_element_by_id('quote-div')
+        time.sleep(5)
+        #the user refreshes the mainpage. A new random quote is displayed.
+        self.fail('finish test')
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore') 
