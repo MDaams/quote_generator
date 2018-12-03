37bd2e69727e465f5ec8b26d9d9e023ca8eae8ce
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-03 20:07:08 +0100

    fixed the project.

--- quote_generator/mainpage/tests.py
+++ quote_generator/mainpage/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from mainpage.views import main_page
 
-# Create your tests here.
+class MainPageTest(TestCase):
+    
+    def test_root_url_returns_main_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, main_page)
