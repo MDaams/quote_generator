65ee1148fdf33c63bed92abca12932a988ece46d
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-09 16:25:33 +0100

    making test pass.

--- quote_generator/mainpage/views.py
+++ quote_generator/mainpage/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'main.html')
+    quote = "this is a quote"
+    quote_data = {"quote": quote}
+    return render(request, 'main.html', quote_data)
