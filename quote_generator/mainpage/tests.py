d07bff36e6610c95cbdc5f7128e6507d94de5334
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-09 16:50:31 +0100

    removed old test.

--- quote_generator/mainpage/tests.py
+++ quote_generator/mainpage/tests.py
@@ -3,11 +3,7 @@ from django.urls import resolve
 from mainpage.views import main_page
 from django.http import request
 
-class MainPageTest(TestCase):
-
-    def test_root_url_returns_mainpage(self):
-        response = resolve('/')
-        self.assertEqual(response.func, main_page)        
+class MainPageTest(TestCase):  
     
     def test_mainpage_template_used(self):
         response = self.client.get('/')
