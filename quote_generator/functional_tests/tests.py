c0fa5412b9dcae6e0b460c30182fd8c6b4cde862
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-09 16:27:18 +0100

    expanded test.

--- quote_generator/functional_tests/tests.py
+++ quote_generator/functional_tests/tests.py
@@ -19,11 +19,15 @@ class NewMainPageVisitorTest(LiveServerTestCase):
         # Page title says 'Quote Generator'
         self.assertIn('Quote Generator', self.browser.title)
         # A random quote is displayed.
-        first_quote = self.browser.find_element_by_id('quote-div').text       
+        first_quote = self.browser.find_element_by_id('quote-div').text
 
         self.assertNotEqual(first_quote, '')
 
         #the user refreshes the mainpage. A new random quote is displayed.
+        self.browser.refresh()
+        second_quote = self.browser.find_element_by_id('quote-div').text
+        self.assertNotEqual(second_quote, '')
+        self.assertNotEqual(second_quote, first_quote, 'Expected a new quote!')
         self.fail('finish test')
 
 if __name__ == '__main__':  
