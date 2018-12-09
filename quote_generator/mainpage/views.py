7f804e3d778955f5fd2f58cb56fba820a3bafe03
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-09 16:32:05 +0100

    quote now requested using api.

--- quote_generator/mainpage/views.py
+++ quote_generator/mainpage/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 def main_page(request):
-    quote = "this is a quote"
-    quote_data = {"quote": quote}
+    response = requests.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1')
+    payload = json.loads(response.text)
+    quote_data = {"quote": payload[0]['content'], "author": payload[0]['title']}
     return render(request, 'main.html', quote_data)
