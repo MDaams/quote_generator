34d3e33ac05019f74b356b64d5727a6e02e5e9d2
Martijn Daams <m.Daams@student.fontys.nl>
2018-12-09 16:49:58 +0100

    refactored tests.

--- quote_generator/mainpage/tests.py
+++ quote_generator/mainpage/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 from django.urls import resolve
 from mainpage.views import main_page
+from django.http import request
 
 class MainPageTest(TestCase):
+
+    def test_root_url_returns_mainpage(self):
+        response = resolve('/')
+        self.assertEqual(response.func, main_page)        
     
-    def test_root_url_returns_main_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, main_page)
+    def test_mainpage_template_used(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'main.html')
